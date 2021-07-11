32a4087b8617dc68dd7724f5f12ed9089de65977
Ray Lin <lin0074@mhs.vic.edu.au>
2021-07-11 18:00:00 +1000

    chore: added react function

---
+++ modules/react.py
@@ -0,0 +1,50 @@
+import asyncio
+
+
+
+# <-----------------[ADD REACTIONS]------------------> #
+
+async def addReactions(ctx, message):
+
+  await message.add_reaction('📈')
+  await message.add_reaction('🗑️')
+
+  while True:
+
+    def check(reaction, user):
+      return (reaction.message.id == message.id 
+        and user.name != ctx.bot.user.name)
+
+    try: 
+
+      reaction, user = await ctx.bot.wait_for(
+        'reaction_add', 
+        timeout = 120,
+        check = check
+      )
+
+      emoji = str(reaction)
+      await message.remove_reaction(reaction, user)
+
+      if emoji == '📈':
+
+        await ctx.invoke(
+          ctx.bot.get_command('graph'),
+          equation = ctx.kwargs['equation'],
+          lower = -5,
+          upper = 5
+        )
+
+      elif emoji == '🗑️':
+
+        await message.delete()
+        await ctx.message.delete()
+
+        return
+
+    except asyncio.TimeoutError:
+        
+      await message.clear_reactions()
+      break
+
+  return
