6d8c1b54aa2da580e0ed74b6ccbd0eede2c2cdaa
Ray Lin <lin0074@mhs.vic.edu.au>
2021-07-12 15:00:00 +1000

    feat: added display command

--- cogs/maths.py
+++ cogs/maths.py
@@ -20,6 +20,31 @@ class cogMaths(commands.Cog):
 
   def __init__(self, bot):
     self.bot = bot
+    
+  
+  
+  # <-----------------[CMD DISPLAY]------------------> #
+
+  @commands.command(
+    name = 'display',
+    description = "Display an equation!",
+    aliases = []
+  )
+  async def cmdDisplay(self, ctx, *, equation):
+
+    start = datetime.now()
+    renderTeX(f"${equation}$")
+    end = datetime.now()
+
+    time_taken = (end - start).total_seconds()
+
+    file = discord.File('tex.png')
+    embed = getThemedEmbed(ctx, equation, time_taken)
+    message = await ctx.send(file=file, embed=embed)
+
+    await addReactions(ctx, message)
+    
+    return
 
     
  
