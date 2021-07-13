7340ef971cc6a606e1f64931bd9e76952e68d659
Ray Lin <lin0074@mhs.vic.edu.au>
2021-07-13 16:00:00 +1000

    feat: added derive command

--- cogs/maths.py
+++ cogs/maths.py
@@ -108,7 +108,32 @@ class cogMaths(commands.Cog):
     return
 
 
+  
+  # <------------------[CMD DERIVE]------------------> #
+
+  @commands.command(
+    name = 'derive',
+    description = "Derive an equation!",
+    aliases = []
+  )
+  async def cmdDerive(self, ctx, equation):
+
+    from modules.maths import getDerivative
+
+    start = datetime.now()
+    getDerivative(equation)
+    end = datetime.now()
+
+    time_taken = (end - start).total_seconds()
+
+    file = discord.File('tex.png')
+    embed = getThemedEmbed(ctx, equation, time_taken)
+    await ctx.send(file=file, embed=embed)
+
+    return
  
+
+
 # adds the Maths commands to the bot
 def setup(bot):
   bot.add_cog(cogMaths(bot))
