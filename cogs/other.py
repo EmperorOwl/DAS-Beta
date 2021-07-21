374722d21883dae99ed944070c0afd0dadc961a2
Ray Lin <lin0074@mhs.vic.edu.au>
2021-07-21 15:00:00 +1000

    feat: added ping command

--- cogs/other.py
+++ cogs/other.py
@@ -12,8 +12,29 @@ class cogOther(commands.Cog):
   def __init__(self, bot):    
     self.bot = bot
 
+    
+   
+  # <-------------------[CMD PING]-------------------> #
 
+  @commands.command(
+    name = 'ping',
+    description = "Check the ping.",
+    aliases = []
+  )
+  async def cmdPing(self, ctx):
 
+    import time
+
+    start = time.perf_counter()
+    msg = await ctx.send(content="Pinging...")
+    end = time.perf_counter()
+    t_t = (end - start) * 1000
+    await msg.edit(content="Pong! {:.2f}ms".format(t_t))
+
+    return
+
+  
+  
   # <-----------------[CMD DBSETUP]------------------> #
 
   @commands.command(
